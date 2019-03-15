e812a33d9c71ac5049fee7fda61ad4990a9cb754
yifanlyu <yifanlyu@utexas.edu>
2019-03-15 13:04:04 -0500

    hw9

---
+++ HWK9/slideshow.js
@@ -0,0 +1,99 @@
+var currentSlide = 1;
+var lastexectime =0;
+var d = new Date();
+var cur = 0;
+'use strict';
+
+$(function() {
+
+    //settings for slider
+    var width = 1080;
+    var animationSpeed = 1000;
+    var pause = 5000;
+
+    //cache DOM elements
+    var $slider = $('.slideshow-container');
+    var $slideContainer = $('.slides', $slider);
+    var $slides = $('.mySlides', $slider);
+
+    var interval;
+
+    var d = new Date();
+    cur=d.getTime();
+    if (cur-lastexectime>animationSpeed*1.5) {
+        function startSlider() {
+            interval = setInterval(function() {
+                var d = new Date();
+                lastexectime=d.getTime();
+                $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+                    ++currentSlide;
+                    if (currentSlide === $slides.length) {
+                        currentSlide = 1;
+                        $slideContainer.css('margin-left', 0);
+                    }
+                });
+            }, pause);
+        }
+    }
+
+    function pauseSlider() {
+        clearInterval(interval);
+    }
+
+    $slideContainer
+    .on('mouseenter', pauseSlider)
+    .on('mouseleave', startSlider);
+
+    function handleVisibilityChange() {
+        if (document.visibilityState == "hidden") {
+        pauseSlider();
+        } else {
+        startSlider();
+    }
+    }
+
+    startSlider();
+
+});
+
+
+function plusSlides(n) {
+    var width = 1080;
+    var animationSpeed = 1000;
+    var pause = 5000;
+
+    var $slider = $('.slideshow-container');
+    var $slideContainer = $('.slides', $slider);
+    var $slides = $('.mySlides', $slider);
+    var d = new Date();
+    cur=d.getTime();
+
+    if (cur-lastexectime>animationSpeed*1.1) {
+        if (n==1) {
+            var d = new Date();
+            lastexectime=d.getTime();
+            $slideContainer.animate({'margin-left': '-='+width*n}, animationSpeed, function() {
+                ++currentSlide;
+                if (currentSlide=== $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            });
+
+        } else if (n==-1) {
+            if (currentSlide === 1) {
+                currentSlide = $slides.length;
+                $slideContainer.css('margin-left', (1-$slides.length)*width);
+                var d = new Date();
+                lastexectime=d.getTime();
+                $slideContainer.animate({'margin-left': '-='+width*n}, animationSpeed);
+                --currentSlide;
+            } else {
+                var d = new Date();
+                lastexectime=d.getTime();
+                $slideContainer.animate({'margin-left': '-='+width*n}, animationSpeed);
+                --currentSlide;
+            }
+        }
+    }
+}
