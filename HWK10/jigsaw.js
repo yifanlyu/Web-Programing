0536388265fdddd82e79fddf50ad4758cda031fd
yifanlyu <yifanlyu@utexas.edu>
2019-03-20 23:28:33 +0100

    hw10-timer

--- HWK10/jigsaw.js
+++ HWK10/jigsaw.js
@@ -18,6 +18,7 @@ function shuffle(a) {
     return a;
 }
 
+// function to generte image
 var imgnum=imgarray[getRandomArbitrary(3)];
 var slicenum=shuffle(slicearray).pop();
 $( ".line1" ).append('<img id='+imgnum +' onmousedown = "grabber(event);" style = "position: absolute; top: 0px; left: 0px;" class="puzzlepiece" src="./img/img'+imgnum+'-'+slicenum+'.jpg" alt="" >');
@@ -44,6 +45,7 @@ $( ".line3" ).append('<img id='+imgnum +' onmousedown = "grabber(event);" style 
 var slicenum=shuffle(slicearray).pop();
 $( ".line3" ).append('<img id='+imgnum +' onmousedown = "grabber(event);" style = "position: absolute; top: 200px; left: 300px;" class="puzzlepiece" src="./img/img'+imgnum+'-'+slicenum+'.jpg" alt="" >');
 
+// *******************************************************
 
 /*
  Define variables for the values computed by the grabber event
@@ -116,3 +118,78 @@ function dropper(event) {
 
   event.stopPropagation();
 }  //** end of dropper
+
+
+// *******************************************************
+// function to verify all positions of the puzzle pieces
+// function Done() {
+//     var right = true
+//     var posX = parseInt(theElement.style.left);
+//     var posY = parseInt(theElement.style.top);
+//     for (var i = 0; i < 12.length; i++) {
+//         theElement=document.getElementById(i)
+//         var posX = parseInt(theElement.style.left);
+//         var posY = parseInt(theElement.style.top);
+//         if (i==1 | posX=0 | posY=0) {
+//             continue
+//         }else if (i==2 | posX==100 | posY==0) {
+//             continue
+//         }else if (i==3 | posX==200 | posY==0) {
+//             continue
+//         }else if (i==4 | posX==300 | posY==0) {
+//             continue
+//         }else if (i==5 | posX==0 | posY==100) {
+//             continue
+//         }else if (i==6 | posX==100 | posY==100) {
+//             continue
+//         }else if (i==7 | posX==200 | posY==100) {
+//             continue
+//         }else if (i==8 | posX==300 | posY==100) {
+//             continue
+//         }else if (i==9 | posX==0 | posY==200) {
+//             continue
+//         }else if (i==10 | posX==100 | posY==200) {
+//             continue
+//         }else if (i==11 | posX==200 | posY==200) {
+//             continue
+//         }else if (i==12 | posX==300 | posY==200) {
+//             continue
+//         }
+//         else {
+//             right=false
+//         }
+//     }
+// }
+
+// *******************************************************
+// function of the timer
+
+// get cuurent time and define variables
+var startTime = Date.now();
+var second = 1000;
+var minute = second * 60;
+var hour = minute * 60;
+var container = document.getElementById('timer');
+
+function stopTimer() {
+    clearInterval(timer);
+}
+
+function pad(n){
+  return ('00' + n).slice(-2);
+}
+
+//calculate the time
+var timer = setInterval(function(){
+  var currentTime = Date.now();
+  var difference = currentTime - startTime;
+
+  var hours = pad((difference / hour) | 0);
+  var minutes = pad(((difference % hour) / minute) | 0);
+  var seconds = pad(((difference % minute) / second) | 0);
+
+  container.innerHTML = hours + ':' + minutes + ':' + seconds;
+
+// This only represents time between renders. Actual time rendered is based
+// on the elapsed time calculated above.
+}, 250);
